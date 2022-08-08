46a38a97cad6daa60ba6f0738b5384d30fae78ba
julesprezrdlf <julioprezrdlf@gmail.com>
2022-08-08 11:42:35 +0200

    forgot name

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Unicorn Startup Simulator',
+    title: 'TechCompany Simulator',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
